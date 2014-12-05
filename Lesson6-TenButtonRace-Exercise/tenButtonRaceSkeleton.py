c62938ec471966f25e933f5ec4c81e200e6994e5
Tracy Han <hyn2000219@gmail.com>
2014-12-05 12:09:42 -0500

    Initial project

--- Lesson6-TenButtonRace-Exercise/tenButtonRaceSkeleton.py
+++ Lesson6-TenButtonRace-Exercise/tenButtonRaceSkeleton.py
@@ -1,27 +1,54 @@
 #! /usr/bin/evn python
 
-#import the graphical library
-#import the time module
+import wx #import the graphical library
+import time #import the time module
+from random import randint
+
 
 class TenButtonFrame(wx.Frame):
 	def __init__(self, parent):
-		wx.Frame.__init__(self, parent, wx.ID_ANY, "Title Here")
+		wx.Frame.__init__(self, parent, wx.ID_ANY, "Ten Button Race")
 		
 		#Make a new Panel
+		self.panel = wx.Panel(self)
 		
 		#Make the start button
+		self.btnStartButton = wx.Button(self.panel, label = "start", pos = (150, 90))
+		self.btnStartButton.Bind(wx.EVT_BUTTON, self.OnStart)
 		#Make the other ten buttons
-		#Hide the other ten buttons
 		
-		#Bind all the buttons to their event handlers
+		self.buttons = []
+		
+		for i in range(10):
+			a = randint(0, 275)
+			b = randint(0, 190)
+			self.buttons.append(wx.Button(self.panel, label = "button " + str(i + 1), pos = (a, b)))
+			self.buttons[i].Show(False)
+			self.buttons[i].Bind(wx.EVT_BUTTON, self.OnButtons)
+
 		
 	# Event handler for the start button
 	def OnStart(self, e):
-		#Make the start button disappear
+		self.btnStartButton.Show(False) #Make the start button disappear
 		self.startTime = time.time()
-		#Make Button One appear
+		self.buttons[0].Show(True) #Make Button One appear
 		
-	#Other event handlers here
+
+	def OnButtons(self, e):
+		clickedButton = e.GetEventObject()
+		clickedButton.Show(False)
+		for i in range(10):
+			if clickedButton == self.buttons[i]:
+				if i <= 8:
+					self.buttons[i + 1].Show(True)
+				if i == 9:
+					self.endTime = time.time()
+					resultTime = self.endTime - self.startTime
+					resultTime = round(resultTime, 2)
+					self.result = wx.StaticText(self.panel, label = \
+					"Your total time to finish click is {}s".format(resultTime), \
+					pos = (76, 7))
+
 	
 	#Remember the last event handler needs to print the final time.
 	
